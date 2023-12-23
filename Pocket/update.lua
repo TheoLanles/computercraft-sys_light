ac87eac8913006afdd565fd58cf2c3d811cae5a2
Theo <62571239+TheoLanles@users.noreply.github.com>
2023-12-24 00:45:04 +0100

    ms

--- Pocket/update.lua
+++ Pocket/update.lua
@@ -29,33 +29,46 @@ local function supprimerFichier(chemin)
 end
 
 -- Supprime chaque fichier local s'il existe
+local suppressionReussie = true
 for _, fichier in ipairs(fichiers) do
-    supprimerFichier(fichier.cheminLocal)
-end
-
--- Télécharge chaque fichier depuis GitHub
-for _, fichier in ipairs(fichiers) do
-    local cheminGitHub = "https://raw.githubusercontent.com/" .. utilisateur .. "/" .. repo .. "/main/" .. fichier.cheminGitHub
-    print("Téléchargement de " .. fichier.cheminGitHub .. "...")
-    local contenu, erreur = http.get(cheminGitHub)
-
-    if contenu then
-        print("Téléchargement de " .. fichier.cheminGitHub .. " réussi.")
-
-        -- Enregistre le contenu téléchargé localement
-        local programme = fs.open(fichier.cheminLocal, "w")
-        if programme then
-            programme.write(contenu.readAll())
-            programme.close()
-            print("Mise à jour du fichier " .. fichier.cheminLocal)
+    if fs.exists(fichier.cheminLocal) then
+        local success, err = pcall(fs.delete, fichier.cheminLocal)
+        if success then
+            print("Suppression de l'ancien fichier " .. fichier.cheminLocal)
         else
-            print("Erreur lors de l'écriture du fichier " .. fichier.cheminLocal)
+            print("Erreur lors de la suppression de l'ancien fichier " .. fichier.cheminLocal .. ": " .. err)
+            suppressionReussie = false
         end
-    else
-        print("Échec du téléchargement de " .. fichier.cheminGitHub .. ": " .. tostring(erreur))
     end
 end
 
+-- Effectue la mise à jour uniquement si la suppression réussit
+if suppressionReussie then
+    -- Télécharge chaque fichier depuis GitHub
+    for _, fichier in ipairs(fichiers) do
+        local cheminGitHub = "https://raw.githubusercontent.com/" .. utilisateur .. "/" .. repo .. "/main/" .. fichier.cheminGitHub
+        print("Téléchargement de " .. fichier.cheminGitHub .. "...")
+        local contenu, erreur = http.get(cheminGitHub)
+
+        if contenu then
+            print("Téléchargement de " .. fichier.cheminGitHub .. " réussi.")
+
+            -- Enregistre le contenu téléchargé localement
+            local programme = fs.open(fichier.cheminLocal, "w")
+            if programme then
+                programme.write(contenu.readAll())
+                programme.close()
+                print("Mise à jour du fichier " .. fichier.cheminLocal)
+            else
+                print("Erreur lors de l'écriture du fichier " .. fichier.cheminLocal)
+            end
+        else
+            print("Échec du téléchargement de " .. fichier.cheminGitHub .. ": " .. tostring(erreur))
+        end
+    end
+else
+    print("La suppression des anciens fichiers a échoué. Aucune mise à jour n'a été effectuée.")
+end
 -- Affiche un message de mise à jour terminée
 print("== Mise à jour terminée ==")
 print("Redémarrage en cours...")
