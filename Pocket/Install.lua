7bec2c4d6b4a7611455398cffec57284975c02cf
Theo <62571239+TheoLanles@users.noreply.github.com>
2023-12-23 23:09:23 +0100

    ms

--- Pocket/Install.lua
+++ Pocket/Install.lua
@@ -3,7 +3,7 @@ local utilisateur = "TheoLanles"
 local repo = "computercraft-sys_light"
 local fichiers = {
     { cheminGitHub = "Pocket/boot.lua", cheminLocal = "/boot.lua" },
-    { cheminGitHub = "Pocket/startup.lua.lua", cheminLocal = "/startup.lua" },
+    { cheminGitHub = "Pocket/startup.lua", cheminLocal = "/startup.lua" },
     { cheminGitHub = "Pocket/client.lua", cheminLocal = "/client.lua" },
     { cheminGitHub = "Pocket/config.lua", cheminLocal = "/config.lua" },
     { cheminGitHub = "Pocket/update.lua", cheminLocal = "/update.lua" },
