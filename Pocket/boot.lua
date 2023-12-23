dc6fda03a88454d4b34162db8666de091b943093
Theo <62571239+TheoLanles@users.noreply.github.com>
2023-12-24 00:27:15 +0100

    ms

--- Pocket/boot.lua
+++ Pocket/boot.lua
@@ -44,7 +44,7 @@ function main()
     while true do
         clearScreen()
         term.setTextColor(textColor)
-        local menuTitle = "  == Mon OS == V.1.3"
+        local menuTitle = "  == Mon OS == V.1.4"
         term.setCursorPos(centerText(menuTitle), 1)
         print(menuTitle)
         print(string.rep("-", 26)) -- Ligne horizontale
