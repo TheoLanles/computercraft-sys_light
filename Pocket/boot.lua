fd1582b50efd34d16ff795d722e2be60d8a832f9
Theo <62571239+TheoLanles@users.noreply.github.com>
2023-12-23 23:26:22 +0100

    ms

--- Pocket/boot.lua
+++ Pocket/boot.lua
@@ -89,7 +89,7 @@ function main()
                 clearScreen()
                 print("Redémarrage en cours...")
                 sleep(2)
-                shell.run("t")
+                shell.run("rebooot")
             elseif selectedOption == 6 then
                 shell.run("update.lua")
                 return
