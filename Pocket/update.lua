1a0be3c432ba19130c78fd38f3094a16bd1687fb
Theo <62571239+TheoLanles@users.noreply.github.com>
2023-12-23 22:06:24 +0100

    ms

--- Pocket/update.lua
+++ Pocket/update.lua
@@ -7,47 +7,41 @@ local textColor = colors.black
   term.setCursorPos(1, 1)
   term.setTextColor(textColor)
 
-
-
-local filesToDownload = {
-    {pastebin = "VQjpEnAa", path = "/boot.lua"},
-    {pastebin = "Cwwjbdu4", path = "/startup.lua"},
-    {pastebin = "R0Cq6TYQ", path = "/client.lua"},
-    {pastebin = "eedPz59Z", path = "/config.lua"},
-    {pastebin = "u3S85kCk", path = "/update.lua"},
-    -- Ajoutez d'autres fichiers si nécessaire
+-- Spécifiez l'utilisateur, le référentiel et la liste des fichiers sur GitHub
+local utilisateur = "TheoLanles"
+local repo = "computercraft-sys_light"
+local fichiers = {
+    { cheminGitHub = "Pocket/boot.lua", cheminLocal = "/boot.lua" },
+    { cheminGitHub = "Pocket/startup.lua.lua", cheminLocal = "/startup.lua" },
+    { cheminGitHub = "Pocket/client.lua", cheminLocal = "/client.lua" },
+    { cheminGitHub = "Pocket/config.lua", cheminLocal = "/config.lua" },
+    { cheminGitHub = "Pocket/update.lua", cheminLocal = "/update.lua" },
+    -- Ajoutez autant de fichiers que nécessaire
 }
- 
--- Fonction pour télécharger un fichier depuis Pastebin
-local function downloadFromPastebin(pastebinId, destinationPath)
-    local url = "https://pastebin.com/raw/" .. pastebinId
-    local response = http.get(url)
- 
-    if response then
-        local content = response.readAll()
-        response.close()
- 
-        local file = fs.open(destinationPath, "w")
-        file.write(content)
-        file.close()
-        return true  -- Téléchargement réussi
+
+-- Télécharge chaque fichier depuis GitHub
+for _, fichier in ipairs(fichiers) do
+    local cheminGitHub = "https://raw.githubusercontent.com/" .. utilisateur .. "/" .. repo .. "/main/" .. fichier.cheminGitHub
+    local contenu, erreur = http.get(cheminGitHub)
+
+    if contenu then
+        print("Téléchargement de " .. fichier.cheminGitHub .. " réussi.")
+
+        -- Enregistre le contenu téléchargé localement
+        local programme = fs.open(fichier.cheminLocal, "w")
+        programme.write(contenu.readAll())
+        programme.close()
+
+        -- Exécute le code du programme
+        local success, err = pcall(dofile, fichier.cheminLocal)
+        if not success then
+            print("Erreur lors de l'exécution de " .. fichier.cheminLocal .. ": " .. err)
+        end
     else
-        return false  -- Échec du téléchargement
+        print("Échec du téléchargement de " .. fichier.cheminGitHub .. ": " .. (erreur or "Erreur inconnue"))
     end
 end
- 
-print("== Début de l'installation ==")
- 
-for _, file in pairs(filesToDownload) do
-    print("Téléchargement depuis Pastebin : " .. file.pastebin)
-    if downloadFromPastebin(file.pastebin, file.path) then
-        print("Téléchargement terminé : " .. file.path)
-    else
-        print("Échec du téléchargement depuis Pastebin : " .. file.pastebin)
-        return  -- Arrête l'installation en cas d'échec
-    end
-end
- 
+
 print("== Mise a jour terminée ==")
 print("Redémarrage en cours...")
 os.sleep(1)  -- Attendez un instant pour afficher le message
