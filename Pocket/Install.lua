e64a45455148c87b3333a0aeff726fc0b91b428f
Theo <62571239+TheoLanles@users.noreply.github.com>
2023-12-23 21:25:03 +0100

    ms structure

--- Pocket/Install.lua
+++ Pocket/Install.lua
@@ -1,3 +1,21 @@
+-- Charge le système de fichiers virtuel (VFS) pour GitHub
+local githubPath = "/lib/vfs/github.lua"
+if not fs.exists(githubPath) then
+    print("Téléchargement de la bibliothèque GitHub VFS...")
+    local githubVfsCode = http.get("https://raw.githubusercontent.com/someuser/someproject/master/lib/vfs/github.lua")
+    if githubVfsCode then
+        local githubVfsFile = fs.open(githubPath, "w")
+        githubVfsFile.write(githubVfsCode.readAll())
+        githubVfsFile.close()
+        githubVfsCode.close()
+    else
+        error("Impossible de télécharger la bibliothèque GitHub VFS.")
+    end
+end
+
+-- Charge la bibliothèque GitHub VFS
+local github = require("/lib/vfs/github")
+
 -- Spécifiez l'utilisateur, le référentiel et la liste des fichiers sur GitHub
 local utilisateur = "TheoLanles"
 local repo = "computercraft-sys_light"
@@ -12,14 +30,18 @@ local fichiers = {
 
 -- Télécharge chaque fichier depuis GitHub
 for _, fichier in ipairs(fichiers) do
-    if github.downloadFile(utilisateur, repo, fichier.cheminGitHub, fichier.cheminLocal) then
+    local cheminLocal = fichier.cheminLocal
+    if github.downloadFile(utilisateur, repo, fichier.cheminGitHub, cheminLocal) then
         print("Téléchargement de " .. fichier.cheminGitHub .. " réussi.")
         -- Charge le fichier téléchargé en tant que programme Lua
-        local programme = fs.open(fichier.cheminLocal, "r")
+        local programme = fs.open(cheminLocal, "r")
         local code = programme.readAll()
         programme.close()
         -- Exécute le code du programme
-        load(code)()
+        local success, err = pcall(load(code))
+        if not success then
+            print("Erreur lors de l'exécution de " .. fichier.cheminGitHub .. ": " .. err)
+        end
     else
         print("Échec du téléchargement de " .. fichier.cheminGitHub)
     end
